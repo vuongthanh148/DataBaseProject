9771ad6f8f712285412964b9279f79029c2ce783
Kenji <48047759+vuongthanh148@users.noreply.github.com>
2021-06-16 22:23:31 +0700

    Updates

--- src/reducers/reducers.js
+++
@@ -1,8 +0,0 @@
-import {combineReducers} from 'redux';
-import indexStateReducer from './index-state-reducer';
-
-var reducers = combineReducers({
-    indexScreen: indexStateReducer
-})
-
-export default reducers;
