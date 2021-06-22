d3f3cdb917f471aec7d2c50223d25ea41f8f6c6c
Kenji <48047759+vuongthanh148@users.noreply.github.com>
2021-06-22 15:33:07 +0700

    Updates

---
+++ precache-manifest.c350fa94c8f66e671cbb765aac1bbcb3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a724b839e7160eed605f1ec00dbef544",
+    "url": "/DatabaseProject/index.html"
+  },
+  {
+    "revision": "948ab2012d21d51f46a6",
+    "url": "/DatabaseProject/static/css/main.01586cb4.chunk.css"
+  },
+  {
+    "revision": "6c06ab4694a89c767dd3",
+    "url": "/DatabaseProject/static/js/2.f30f3a61.chunk.js"
+  },
+  {
+    "revision": "e0c192547816ef7be36ef6ba4b9d10df",
+    "url": "/DatabaseProject/static/js/2.f30f3a61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "948ab2012d21d51f46a6",
+    "url": "/DatabaseProject/static/js/main.25c8dc58.chunk.js"
+  },
+  {
+    "revision": "f4595ae4a4a72f869a43",
+    "url": "/DatabaseProject/static/js/runtime-main.4c1c6bfe.js"
+  },
+  {
+    "revision": "6363e4d5e3a136e94f61c20ff7fda23c",
+    "url": "/DatabaseProject/static/media/cool-background.6363e4d5.png"
+  },
+  {
+    "revision": "c109719f1916037f2cb9ddb4411ebf55",
+    "url": "/DatabaseProject/static/media/exam.c109719f.jpg"
+  },
+  {
+    "revision": "ee5bba7d314b46e8347758100541b149",
+    "url": "/DatabaseProject/static/media/exam_b.ee5bba7d.jpg"
+  },
+  {
+    "revision": "80b5e47ffd72b40c11c2c1efef14182c",
+    "url": "/DatabaseProject/static/media/lecturer2.80b5e47f.jpeg"
+  },
+  {
+    "revision": "bfed14ffa7523b3bf65737576e6369c5",
+    "url": "/DatabaseProject/static/media/student2.bfed14ff.jpg"
+  },
+  {
+    "revision": "ddc02adecf789c27939ccf2383a0e703",
+    "url": "/DatabaseProject/static/media/study.ddc02ade.jpg"
+  },
+  {
+    "revision": "cb8f7ce151763dc375fffad8ae9a4e2e",
+    "url": "/DatabaseProject/static/media/study_b.cb8f7ce1.jpg"
+  }
+]);
